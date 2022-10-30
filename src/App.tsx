d2dd23874ac385cc9e34066133fcce2df188c4a1
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-10-30 22:46:15 +0530

    moved to react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App({ }) {
+  return (
+    <div>
+      <h1 className='text-indigo-400 text-4xl'> Pomodero Technique </h1>
+    </div>
+
+  )
+}
+
+export default App
